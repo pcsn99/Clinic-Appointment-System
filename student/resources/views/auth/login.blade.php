054df564623e43090e69b4b26b9b140ba199e0a9
Iline Gutierrez <20220024963@my.xu.edu.ph>
2025-04-13 21:25:20 +0800

    login h1 and h2 commit

--- student/resources/views/auth/login.blade.php
+++ student/resources/views/auth/login.blade.php
@@ -2,12 +2,35 @@
 
 @section('content')
 <style>
-    {
+    *{
       box-sizing: border-box;
       margin: 0;
       padding: 0;
       font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
     }
+
+    .login-container {
+      background-color: rgba(255, 255, 255, 0.85);
+      padding: 40px;
+      border-radius: 30px;
+      width: 400px;
+      text-align: center;
+      box-shadow: 0 8px 16px rgba(0, 0, 0, 0.25);
+    }
+
+    h1 {
+      font-size: 28px;
+      color: #1A237E; /* Dark Blue */
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+
+    h2 {
+      font-size: 18px;
+      color: #3949AB; /* Slightly Lighter Blue */
+      margin-bottom: 30px;
+    }
+    
 </style>
 
     @if($errors->any())
